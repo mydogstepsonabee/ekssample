97998763a1b8e268da6d598f0a614919f6130ac2
mydogstepsonabee <107633825+mydogstepsonabee@users.noreply.github.com>
2023-06-10 13:56:13 +0900

    modify node number again

--- node-groups.tf
+++ node-groups.tf
@@ -6,8 +6,8 @@ resource "aws_eks_node_group" "this" {
   subnet_ids      = aws_subnet.private[*].id
 
   scaling_config {
-    desired_size = 4
-    max_size     = 5
+    desired_size = 7
+    max_size     = 8
     min_size     = 1
   }
 
